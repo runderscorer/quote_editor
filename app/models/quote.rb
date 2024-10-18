d6b00cb81600a8764c53f81a78e880e1aa7f3ec7
Rick Rieta <rickrieta@gmail.com>
2024-10-18 11:53:37 -0700

    Scope stream to current company to patch security concerns

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,7 +3,7 @@ class Quote < ApplicationRecord
 
   belongs_to :company
 
-  after_create_commit -> { broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quote: self }, target: 'quotes' }
+  # after_create_commit -> { broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quote: self }, target: 'quotes' }
   # This can be moved into a background job by using broadcast_prepend_later_to
   #
   # Can be shortened to: after_create_commit -> { broadcast_prepend_to 'quotes' } if using Rails conventions
@@ -11,11 +11,12 @@ class Quote < ApplicationRecord
   # partial defaults to instance.to_partial_path (e.g., 'quotes/quote')
   # locals defaults to { model_name.element.to_sym => self } (e.g., { quote: self })
   # target defaults to model_name.plural (e.g., 'quotes')
-  after_update_commit -> { broadcast_replace_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: self }
+  # after_update_commit -> { broadcast_replace_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: self }
   # Can also be shortened to: after_create_commit -> { broadcast_replace_to 'quotes' } if using Rails conventions
-  after_destroy_commit -> { broadcast_remove_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: self }
+  # after_destroy_commit -> { broadcast_remove_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: self }
   #
   # And all of these can be combined into one method: broadcasts_to -> (quote) { 'quotes' }, inserts_by: :prepend
+  broadcasts_to -> (quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
 
   scope :ordered, -> { order(created_at: :desc) }
 end
